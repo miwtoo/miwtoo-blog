e7f1a7777847844902972f5a47f35ff22e17bb22
Wongsakorn Chugasem <miwyugioh@gmail.com>
2020-05-18 00:06:41 +0700

    add : starter blog with md

---
+++ src/blogs/test1.md
@@ -0,0 +1,7 @@
+---
+path: "/blog/my-first-post"
+date: "2019-05-07"
+title: "My first blog post"
+---
+
+Oooooh-weeee, my first blog post!
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,44 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
+import PostLink from "../components/post-link"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) => {
+  const Posts = edges
+    .filter(edges => !!edges.node.frontmatter.date)
+    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+
+  return (
+    <Layout>
+      {Posts}
+      <br />
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      limit: 1000
+    ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`
